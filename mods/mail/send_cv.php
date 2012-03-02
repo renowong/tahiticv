83e1d2d004d4bcd17168bd54fcbd006fdd4f7b71
renowong@gmail.com <renowong@gmail.com@1954446d-63f7-26cf-d5b3-3c65fab5c8bc>
2012-03-02 06:05:19 +0000

    still working on emailing cv, need to work next on login in the user automatically

--- mods/mail/send_cv.php
+++ mods/mail/send_cv.php
@@ -1,41 +1,42 @@
 <?php
 set_include_path('/home/renowong/php');
+set_include_path('/usr/share/php');
+
 include_once("../../includes/global_vars.php");
 include_once("../../includes/security.php");
 require_once "Mail.php";
-require_once('Mail/mime.php');
 
 
 $announce_id = $_POST["id"];
 $id = $_COOKIE["user"];
 
-//       $token = md5(uniqid(rand(),1));
-//       
-//       auth($token,$id,$auth_id);
-//       
-//       $rs = getrs($id);
-//       
+       $token = md5(uniqid(rand(),1));
+       
+       auth($token,$id,0);
+      
+       $name = getname($id);
+       $title = gettitle($announce_id);
+     
        $from = "administrateur@tahiticv.com";
        $to = getmail($announce_id);
        $subject = "Soumission d'un CV : TahitiCV";
-       $body = "Bonjour, ceci est un CV de la part de\n";
-//       $body .= "Celle-ci souhaiterait accéder à vos informations personnelles sur votre Curriculum Vitae.\n";
-//       $body .= "Si vous souhaitez donner cette autorisation à $rs, veuillez cliquer sur le lien ci-après :\n";
-//       $body .= SITEADDR."auth.php?token=$token\n";
-//       $body .= "Dans le cas contraire, veuillez ignorer cette demande.\n\n";
-//       $body .= "L'administrateur TahitiCV.\n";
-//
+       $body = "Bonjour, ceci est un envoi de CV de la part de $name. pour votre annonce intitulée \"$title\"\n";
+       $body .= "Si vous souhaitez accéder au CV, veuillez cliquer sur le lien ci-après :\n";
+       $body .= SITEADDR."index.php?auth=$token\n";
+       $body .= "Dans le cas contraire, veuillez ignorer cette demande.\n\n";
+       $body .= "L'administrateur TahitiCV.\n";
+
 
        //old gmail account
-       //$host = "ssl://smtp.gmail.com";
-       //$port = "465";
-       //$username = "tahiticv@gmail.com";
-       //$password = "johnnyte\$t";
+       $host = "ssl://smtp.gmail.com";
+       $port = "465";
+       $username = "tahiticv@gmail.com";
+       $password = "johnnyte\$t";
        
-       $host = "mail.tahiticv.com";
-       $port = "25";
-       $username = "administrateur+tahiticv.com";
-       $password = "jerome12";
+       //$host = "mail.tahiticv.com";
+       //$port = "25";
+       //$username = "administrateur+tahiticv.com";
+       //$password = "jerome12";
        
        $headers = array ('From' => $from,
          'To' => $to,
@@ -52,7 +53,7 @@ $id = $_COOKIE["user"];
        if (PEAR::isError($mail)) {
               echo("<p>" . $mail->getMessage() . "</p>");
        } else {
-              print("Succ\351s de l'envoi!");
+              print("Succes de l'envoi!");
        }
 
 
@@ -80,7 +81,7 @@ function getmail($id){
     return $email;
 }
 
-function getrs($id){
+function getname($id){
        $mysqli = new mysqli(HOST, DBUSER, DBPASSWORD, DB);
 
     /* check connection */
@@ -90,13 +91,32 @@ function getrs($id){
         exit();
     }
     
-    $query = "SELECT `users`.`rs` FROM `users` WHERE `users`.`ID` = '$id'";
+    $query = "SELECT `cv`.`nom`,`cv`.`prenom` FROM `cv` WHERE `cv`.`id_user` = '$id'";
     $result = $mysqli->query($query);
     $row = $result->fetch_row();
-    $rs = $row[0];
+    $n = $row[0]." ".$row[1];
     $result->close();
     $mysqli->close();
-    return $rs;
+    return $n;
+}
+
+function gettitle($id){
+       $mysqli = new mysqli(HOST, DBUSER, DBPASSWORD, DB);
+
+    /* check connection */
+    if (mysqli_connect_errno()) {
+        //printf("Connect failed: %s\n", mysqli_connect_error());
+        print("<?xml version='1.0' encoding='utf-8' ?><!DOCTYPE response SYSTEM 'response.dtd' [<!ENTITY eacute '&#233;'><!ENTITY agrave '&#224;'>]><response success='0' msg='Erreur de connexion &agrave; la base de donn&eacute;es'></response>");
+        exit();
+    }
+    
+    $query = "SELECT `annonces`.`titre` FROM `annonces` WHERE `annonces`.`ID` = '$id'";
+    $result = $mysqli->query($query);
+    $row = $result->fetch_row();
+    $n = $row[0];
+    $result->close();
+    $mysqli->close();
+    return $n;
 }
 
 function auth($token,$id,$auth_id){
