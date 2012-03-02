0b22a9758db129258c8a5f39a65477a910491a64
renowong@gmail.com <renowong@gmail.com@1954446d-63f7-26cf-d5b3-3c65fab5c8bc>
2012-03-02 20:43:42 +0000

    ok, sending works, now need to work on autologin

--- mods/mail/send_cv.php
+++ mods/mail/send_cv.php
@@ -11,14 +11,16 @@ $announce_id = $_POST["id"];
 $id = $_COOKIE["user"];
 
        $token = md5(uniqid(rand(),1));
-       
-       auth($token,$id,0);
+       $ar_annonceur = getdata($announce_id);
+       auth($token,$id,$ar_annonceur[0]);
+       addtoauth($ar_annonceur[0],$id);
       
        $name = getname($id);
        $title = gettitle($announce_id);
+       
      
        $from = "administrateur@tahiticv.com";
-       $to = getmail($announce_id);
+       $to = $ar_annonceur[1];
        $subject = "Soumission d'un CV : TahitiCV";
        $body = "Bonjour, ceci est un envoi de CV de la part de $name. pour votre annonce intitulée \"$title\"\n";
        $body .= "Si vous souhaitez accéder au CV, veuillez cliquer sur le lien ci-après :\n";
@@ -57,9 +59,8 @@ $id = $_COOKIE["user"];
        }
 
 
-function getmail($id){
+function getdata($id){
        $mysqli = new mysqli(HOST, DBUSER, DBPASSWORD, DB);
-
     /* check connection */
     if (mysqli_connect_errno()) {
         //printf("Connect failed: %s\n", mysqli_connect_error());
@@ -71,6 +72,7 @@ function getmail($id){
     $result = $mysqli->query($query);
     $row = $result->fetch_row();
     $annonceurid = $row[0];
+    $array = array($annonceurid);
     
     $query = "SELECT `users`.`email` FROM `users` WHERE `users`.`ID` = '$annonceurid'";
     $result = $mysqli->query($query);
@@ -78,7 +80,8 @@ function getmail($id){
     $email = $row[0];
     $result->close();
     $mysqli->close();
-    return $email;
+    array_push($array,$email);
+    return $array;
 }
 
 function getname($id){
@@ -133,6 +136,32 @@ function auth($token,$id,$auth_id){
         $query = "INSERT INTO `".DB."`.`authorisations` (`ID`, `code`, `id_user`, `id_auth`, `date`) VALUES (NULL, '$token', '$id', '$auth_id', '$date')";
         $mysqli->query($query);
         $mysqli->close();
-    }
+}
 
-    ?>
+function addtoauth($id,$cvid){
+       $mysqli = new mysqli(HOST, DBUSER, DBPASSWORD, DB);
+           /* check connection */
+       if (mysqli_connect_errno()) {
+	   //printf("Connect failed: %s\n", mysqli_connect_error());
+	   print("<?xml version='1.0' encoding='utf-8' ?><!DOCTYPE response SYSTEM 'response.dtd' [<!ENTITY eacute '&#233;'><!ENTITY agrave '&#224;'>]><response success='0' msg='Erreur de connexion &agrave; la base de donn&eacute;es'></response>");
+	   exit();
+       }
+       $query = "SELECT `cv`.`auth_ids` FROM `cv` WHERE `id_user` = '$cvid'";
+        $result = $mysqli->query($query);
+        $row = $result->fetch_row();
+        $auth_ids = $row[0];
+        $result->close();
+	
+	
+        if(strpos($auth_ids, "|".$id."|") === false){
+	      if(substr($auth_ids, -1)!=="|"){$auth_ids .= "|";}
+	       $auth_ids .= $id."|";
+	       $query = "UPDATE `cv` SET `auth_ids` = '$auth_ids' WHERE `id_user` = '$cvid'";
+	       $mysqli->query($query);
+       
+	       $mysqli->close();
+	}
+
+}
+
+?>
